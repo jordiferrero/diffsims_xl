b5f5b0914cf9623ee68a8ed733c4865b06f6eef9
Jordi <jordiferrer97@gmail.com>
2022-06-08 16:05:06 +0100

    Run dagster daemon

--- ml_difsims/simulators/create_json_sim_files.py
+++ ml_difsims/simulators/create_json_sim_files.py
@@ -1,4 +1,5 @@
 import os.path
+import shutil
 
 import tqdm
 from dagster import job, op, static_partitioned_config
@@ -9,6 +10,14 @@ from paramspace import yaml
 
 root = os.path.abspath(r"C:\Users\Sauron\Documents\GitHub\strankslab\ml_difsims")
 path_to_save_jsons = os.path.join(root, "data\simulations\json_files")
+
+# Create folder
+save_folder_path = os.path.join(root, path_to_save_jsons)
+if os.path.exists(save_folder_path):
+    shutil.rmtree(save_folder_path)
+
+if not os.path.exists(save_folder_path):
+    os.makedirs(save_folder_path)
 
 yaml_path = os.path.join(root, "ml_difsims/simulators/param_space_yaml.yml")
 with open(yaml_path, mode='r') as cfg_file:
@@ -23,23 +32,11 @@ print(pspace.get_info_str())
 print("Starting simulation")
 JSON_FILES = []
 for i, params in tqdm.tqdm(enumerate(pspace)):
+    fname = f'json_{i:03}.json'
+    fpath = os.path.join(path_to_save_jsons, fname)
+    with open(fpath, 'w') as f:
+        json.dump(params, f)
     JSON_FILES.append(json.dumps(params))
 
-
-@static_partitioned_config(partition_keys=JSON_FILES)
-def json_md_config(partition_key: str):
-    return {'ops': {'simulate_diffraction': {'ops': {'load_json_metadata_to_dict': {'config': {'json_string': partition_key}}}}},
-            #"execution": {"config": {"multiprocess": {"max_concurrent": 4,}}}
-            }
-
-
-# Write the job
-@job(op_retry_policy=default_policy, config=json_md_config)
-def simulate_diffraction_partitioned():
-    simulate_diffraction()
-
-
-if __name__ == "__main__":
-    result = simulate_diffraction_partitioned.execute_in_process()
-
-
+print(len(JSON_FILES))
+print(JSON_FILES[-1])

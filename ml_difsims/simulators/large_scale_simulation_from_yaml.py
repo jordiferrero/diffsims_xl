86363857a014669733c053f3daaaf1b89a9d4350
Jordi Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2022-06-08 14:13:07 +0100

    Final state

--- ml_difsims/simulators/large_scale_simulation_from_yaml.py
+++ ml_difsims/simulators/large_scale_simulation_from_yaml.py
@@ -1,4 +1,6 @@
 import os.path
+import shutil
+
 import tqdm
 from dagster import job, op, static_partitioned_config
 from ml_difsims.simulators.simulator_diffsims_dagster_ops import *
@@ -8,6 +10,14 @@ from paramspace import yaml
 
 root = os.path.abspath(r"G:\My Drive\PhD\projects\external_measurements\ml_difsims")
 path_to_save_jsons = os.path.join(root, "data\simulations\json_files")
+
+# Create folder
+save_folder_path = os.path.join(root, path_to_save_jsons)
+if os.path.exists(save_folder_path):
+    shutil.rmtree(save_folder_path)
+
+if not os.path.exists(save_folder_path):
+    os.makedirs(save_folder_path)
 
 yaml_path = os.path.join(root, "ml_difsims/simulators/param_space_yaml.yml")
 with open(yaml_path, mode='r') as cfg_file:
@@ -22,15 +32,14 @@ print(pspace.get_info_str())
 print("Starting simulation")
 JSON_FILES = []
 for i, params in tqdm.tqdm(enumerate(pspace)):
-    # fname = f'json_{i:05}.json'
-    # fpath = os.path.join(path_to_save_jsons, fname)
-    # with open(fpath, 'w') as f:
-    #     json.dump(params, f)
+    fname = f'json_{i:03}.json'
+    fpath = os.path.join(path_to_save_jsons, fname)
+    with open(fpath, 'w') as f:
+        json.dump(params, f)
     JSON_FILES.append(json.dumps(params))
-    if i == 3:
-        break
 
-print(JSON_FILES)
+print(len(JSON_FILES))
+print(JSON_FILES[-1])
 
 
 @static_partitioned_config(partition_keys=JSON_FILES)
